6cd483fa5bc18b071630a8a37c0396eaef5b8696
echeng <echeng@ci.com>
2017-07-14 17:05:30 -0400

    add some test on basic test case.

--- src/main/webapp/script/basic.js
+++ src/main/webapp/script/basic.js
@@ -3,3 +3,6 @@ myHeading.textContent = 'Hello world!';
 document.querySelector('html').onclick = function() {
     alert('Ouch! Stop poking me!');
 }
+
+var a;
+document.getElementById("demo1").innerHTML = a;
