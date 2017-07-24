73d943c9ddf11160dccfdfe137a08a2fa61affd1
echeng <echeng@ci.com>
2017-07-24 14:29:35 -0400

    add some test cases.

---
+++ src/main/webapp/script/operators.js
@@ -0,0 +1,15 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'operators';
+
+
+var a = [1,2,3];
+var b = [1,2,3];
+
+var c = (a == b);
+document.getElementById("demo1").innerHTML = c;
+
+var d = "a";
+var e = 5;
+var f = new Array("a","b","c");
+
+document.getElementById("demo2").innerHTML = a.constructor == Array;
